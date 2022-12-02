177aff74cb2e405f3643255b22eaa51177cec568
Jensen Bradshaw <jensenbradshaw.jb@gmail.com>
2022-12-02 13:44:41 +0000

    Added C solution for day 1

---
+++ 22/day1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void main()
+{
+    FILE* fp = fopen("day1.txt", "r");
+
+    int current_calories = 0;
+    int highest_calories = 0;
+    int second_highest_calories = 0;
+    int third_highest_calories = 0;
+
+    char line[10];
+    while(fgets(line, 10, fp))
+    {
+        if( line[0] == '\n' || line[0] == '\r' )
+        {
+            if( highest_calories < current_calories )
+            {
+                third_highest_calories = second_highest_calories;
+                second_highest_calories = highest_calories;
+                highest_calories = current_calories;
+            }
+            else if( second_highest_calories < current_calories )
+            {
+                third_highest_calories = second_highest_calories;
+                second_highest_calories = current_calories;
+            }
+            else if( third_highest_calories < current_calories )
+            {
+                third_highest_calories = current_calories;
+            }
+            current_calories = 0;
+        }
+        else
+        {
+            current_calories += atoi(line);
+        }
+    }
+
+    printf("Part 1 Solution: %d\n", highest_calories);
+    printf("Part 2 Solution: %d\n", highest_calories + second_highest_calories + third_highest_calories);
+
+    fclose(fp);
+
+}
