a9f1f976841f59f032396c93176b7e970d8a85c9
Jensen Bradshaw <jensenbradshaw.jb@gmail.com>
2022-12-02 14:17:49 +0000

    Cleaned up code

--- 22/day1.c
+++ 22/day1.c
@@ -5,29 +5,27 @@ void main()
     FILE* fp = fopen("day1.txt", "r");
 
     int current_calories = 0;
-    int highest_calories = 0;
-    int second_highest_calories = 0;
-    int third_highest_calories = 0;
-
+    int top[] = {0, 0, 0};
     char line[10];
+    
     while(fgets(line, 10, fp))
     {
-        if( line[0] == '\n' )
+        if( line[0] == '\n' || line[0] == '\r' )
         {
-            if( highest_calories < current_calories )
+            if( top[0] < current_calories )
             {
-                third_highest_calories = second_highest_calories;
-                second_highest_calories = highest_calories;
-                highest_calories = current_calories;
+                top[2] = top[1];
+                top[1] = top[0];
+                top[0] = current_calories;
             }
-            else if( second_highest_calories < current_calories )
+            else if( top[1] < current_calories )
             {
-                third_highest_calories = second_highest_calories;
-                second_highest_calories = current_calories;
+                top[2] = top[1];
+                top[1] = current_calories;
             }
-            else if( third_highest_calories < current_calories )
+            else if( top[2] < current_calories )
             {
-                third_highest_calories = current_calories;
+                top[2] = current_calories;
             }
             current_calories = 0;
         }
@@ -37,8 +35,8 @@ void main()
         }
     }
 
-    printf("Part 1 Solution: %d\n", highest_calories);
-    printf("Part 2 Solution: %d\n", highest_calories + second_highest_calories + third_highest_calories);
+    printf("Part 1 Solution: %d\n", top[0]);
+    printf("Part 2 Solution: %d\n", top[0] + top[1] + top[2]);
 
     fclose(fp);
 
