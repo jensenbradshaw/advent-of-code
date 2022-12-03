f4bf4a158ef83aae0f6e2946dd9be8ce73a76327
Jensen Bradshaw <jensenbradshaw.jb@gmail.com>
2022-12-03 11:51:08 +0000

    Full solutions for 22 day 3 in Python3

--- 22/day3.py
+++ 22/day3.py
@@ -2,19 +2,40 @@ f = open('day3.txt')
 lines= f.readlines()
 f.close()
 
-priority_sum = 0
+def getPriority(char):
+    if char.isupper():
+        return (ord(char) - 38)
+    else:
+        return (ord(char) - 96)
 
-for line in lines:
-    length = len(line)//2
-    comp1 = line[:length]
-    comp2 = line[length:]
+def solution_1(lines):
+    priority_sum = 0
 
-    for char in comp2:
-        if char in comp1:
-            if char.isupper():
-                priority_sum += (ord(char) - 38)
-            else:
-                priority_sum += (ord(char) - 96)
-            break
+    for line in lines:
+        length = len(line)//2
+        s1 = set(line[:length])
+        s2 = set(line[length:])
+        priority_sum += getPriority(list(s1.intersection(s2))[0])
 
-print("Solution 1:", priority_sum)
+    return priority_sum
+
+def solution_2(lines):
+    priority_sum = 0
+    s1 = set()
+    s2 = set()
+    s3 = set()
+
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace('\n', '')
+        if i%3 == 0:
+            s1 = set(lines[i])
+        elif i%3 == 1:
+            s2 = set(lines[i])
+        elif i%3 == 2:
+            s3 = set(lines[i])
+            priority_sum += getPriority(list(s1.intersection(s2).intersection(s3))[0])
+
+    return priority_sum
+
+print("Solution 1:", solution_1(lines))
+print("Solution 2:", solution_2(lines))
