33ef583065dd1b08d73540229a30e0a44802c429
Jensen Bradshaw <jensenbradshaw.jb@gmail.com>
2022-12-06 09:42:01 +0000

    Python3 solution for 22 day 6

---
+++ 22/day6.py
@@ -0,0 +1,25 @@
+import re
+
+def openFile(filename):
+    '''Takes in a string containing a file name,
+    returns an array of the lines of the file.'''
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+
+    return lines
+
+def solution(line, size):
+    marker = []
+
+    for i in range(len(line)):
+        if len(marker) == size:
+            if len(set(marker)) == size:
+                return i
+            marker.pop(0)
+        marker.append(line[i])
+
+f = openFile('day6.txt')
+
+print("Solution 1:", solution(f[0], 4))
+print("Solution 2:", solution(f[0], 14))
